6cfbd21e35863fa5f182634d6768e08dc55839bb
Torin Rittenhouse <torin9rittenhouse@gmail.com>
2020-11-13 13:36:55 -0500

    Add + icon as a FAB and started work on IssueCreateFragment

---
+++ app/src/main/java/com/example/environmental_codefest/ui/main/IssueCreateFragment.kt
@@ -0,0 +1,12 @@
+package com.example.environmental_codefest.ui.main
+
+import androidx.fragment.app.Fragment
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class IssueCreateFragment : Fragment() {
+    companion object {
+        val TAG = "IssueCreateFragment"
+    }
+
+}
